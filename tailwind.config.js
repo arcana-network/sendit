2b77cdc1fee33158a088e1608ee24f27ac06d1ff
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-06-23 13:51:03 +0530

    add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ export default {
     fontFamily: {
       onest: ["Onest", "sans-serif"],
     },
+    extend: {
+      colors: {
+        "eerie-black": "#1F1F1F",
+        "philippine-gray": "#8D8D8D",
+        "dark-liver": "#4D4D4D",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
