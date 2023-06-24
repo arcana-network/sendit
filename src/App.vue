70e278266ce2089722df08c822f32c63c0189607
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-06-24 18:31:12 +0530

    initialize auth, add router view & loading screen

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,32 @@
-<!-- <script setup lang="ts">
-</script> -->
+<script setup lang="ts">
+import { onMounted } from "vue";
+import useArcanaAuth from "./use/arcanaAuth";
+import { userLoaderStore } from "./store/loader";
+import FullScreenLoader from "./components/fullScreenLoader.vue";
+import { useRouter } from "vue-router";
+
+const loaderStore = userLoaderStore();
+const router = useRouter();
+
+async function initAuth() {
+  loaderStore.showLoader("initializing...");
+  try {
+    const auth = useArcanaAuth();
+    await auth.init();
+    router.push({ name: "Login" });
+  } catch (error) {
+    console.error({ error });
+  } finally {
+    loaderStore.hideLoader();
+  }
+}
+
+onMounted(initAuth);
+</script>
 
 <template>
-  <div class="bg-black">
-    <p class="text-6xl">Send it</p>
-  </div>
+  <main class="bg-black h-[100vh]">
+    <FullScreenLoader v-if="loaderStore.show" />
+    <router-view> </router-view>
+  </main>
 </template>
