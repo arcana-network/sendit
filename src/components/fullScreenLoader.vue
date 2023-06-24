0d64453cdd208160e089cd132ff3df6ce62a5519
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-06-24 18:23:17 +0530

    create full screen loader

---
+++ src/components/fullScreenLoader.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Overlay from "../components/overlay.vue";
+import { userLoaderStore } from "../store/loader";
+import { HollowDotsSpinner } from "epic-spinners";
+
+const loaderStore = userLoaderStore();
+</script>
+
+<template>
+  <Overlay>
+    <div class="h-full flex flex-col justify-center items-center space-y-10">
+      <HollowDotsSpinner
+        :animation-duration="1000"
+        :dot-size="30"
+        color="#E76124"
+      />
+      <p class="text-xl">{{ loaderStore.message }}</p>
+    </div>
+  </Overlay>
+</template>
