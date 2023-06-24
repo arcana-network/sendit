4abd0abc4cdbde1c3da4236774579ced0ff71c01
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-06-24 18:30:29 +0530

    create transaction page - with no code

---
+++ src/pages/private/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+console.log("private");
+</script>
+
+<template>
+  <router-view></router-view>
+</template>
---
+++ src/pages/private/transaction/send/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+console.log("send");
+</script>
+
+<template>
+  <p>send</p>
+</template>
