3006202a507cdecf8e4c34fd647f98810b88f0e1
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-06-26 00:13:55 +0530

    remove spinner code

--- src/components/fullScreenLoader.vue
+++ src/components/fullScreenLoader.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import Overlay from "../components/overlay.vue";
 import { userLoaderStore } from "../stores/loader";
-import { HollowDotsSpinner } from "epic-spinners";
 
 const loaderStore = userLoaderStore();
 </script>
@@ -9,11 +8,6 @@ const loaderStore = userLoaderStore();
 <template>
   <Overlay>
     <div class="h-full flex flex-col justify-center items-center space-y-10">
-      <HollowDotsSpinner
-        :animation-duration="1000"
-        :dot-size="30"
-        color="#E76124"
-      />
       <p class="text-xl">{{ loaderStore.message }}</p>
     </div>
   </Overlay>
