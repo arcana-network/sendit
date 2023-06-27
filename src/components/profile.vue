c9761620074d70aea079b9d21db07bbe6839ad98
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-06-27 23:29:14 +0530

    create profie card

---
+++ src/components/profile.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="form-card p-0 space-y-[1px] w-80 divide-y-[1px] divide-philippine-gray"
+  >
+    <div class="w-full p-4">
+      <span class="text-base uppercase font-bold">Profile</span>
+    </div>
+    <div class="space-y-4 w-full p-4">
+      <div class="flex flex-col">
+        <span class="text-philippine-gray text-xs">Name</span>
+        <span class="text-sm">Karthik Nathan</span>
+      </div>
+      <div class="flex flex-col">
+        <span class="text-philippine-gray text-xs">Email ID</span>
+        <span class="text-sm">karthiknathan@gmail.com</span>
+      </div>
+      <div class="flex flex-col">
+        <span class="text-philippine-gray text-xs">Account Address</span>
+        <span class="text-sm">9i57i160.... 3738266</span>
+      </div>
+      <div class="w-full">
+        <button class="btn btn-submit-secondary text-sm w-full">Logout</button>
+      </div>
+    </div>
+  </div>
+</template>
