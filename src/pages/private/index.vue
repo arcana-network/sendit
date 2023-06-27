02f4294913138a3cc6aeda7f9fbf460eb5562de7
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-06-27 22:45:57 +0530

    set height to 100vh

--- src/pages/private/index.vue
+++ src/pages/private/index.vue
@@ -4,7 +4,7 @@ import Footer from "../../components/layout/Footer.vue";
 </script>
 
 <template>
-  <div class="h-full flex flex-col">
+  <div class="flex flex-col h-[100vh]">
     <Header />
     <div class="flex-1">
       <router-view></router-view>
