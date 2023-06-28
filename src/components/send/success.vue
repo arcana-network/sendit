45530683e09e29d951b06a05ffcc0693e6932875
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-06-28 21:14:40 +0530

    add styles and tweet link to shoutout

--- src/components/send/success.vue
+++ src/components/send/success.vue
@@ -17,10 +17,17 @@ import SuccessTick from "../../assets/images/icons/success-tick.svg";
     <div class="p-4 h-16 bg-chinese-black w-full rounded-md"></div>
     <div class="flex flex-col space-y-3 w-full">
       <button class="btn btn-submit w-full">SEND on twitter DM</button>
-      <button class="btn btn-submit-secondary w-full">
+      <a
+        class="btn btn-submit-secondary flex justify-center space-x-2 w-full text-center"
+        href="https://twitter.com/intent/tweet?text=Hello%20world"
+        target="_blank"
+      >
         <span>SHOUTOUT on twitter</span>
-        <span>Earn 40 XP</span>
-      </button>
+        <span
+          class="text-cornflower-blue text-xs font-light bg-feep-koamaru p-1 rounded-md"
+          >Earn 40 XP</span
+        >
+      </a>
     </div>
   </div>
 </template>
