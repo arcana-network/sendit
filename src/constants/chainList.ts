427c53e9324e3614f431244978ce82d7d20ce2c2
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-06-29 10:56:06 +0530

    create chain list file

---
+++ src/constants/chainList.ts
@@ -0,0 +1,22 @@
+const chains = [
+  {
+    name: "Ethereum",
+    chain_id: 1,
+    rpc_url: "https://rpc.builder0x69.io",
+    block_chain: "eth",
+  },
+  {
+    name: "Polygon",
+    chain_id: 137,
+    rpc_url: "https://polygon-bor.publicnode.com",
+    block_chain: "polygon",
+  },
+  {
+    name: "Polygon Mumbai",
+    chain_id: 80001,
+    rpc_url: "https://rpc-mumbai.maticvigil.com",
+    block_chain: "polygon_mumbai",
+  },
+];
+
+export default chains;
