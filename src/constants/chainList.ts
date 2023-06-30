ba471f6bea5784fcff5f1ea30e37405198fa100e
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-06-30 13:53:21 +0530

    change it to object

--- src/constants/chainList.ts
+++ src/constants/chainList.ts
@@ -1,22 +1,22 @@
-const chains = [
-  {
+const chains = {
+  1: {
     name: "Ethereum",
     chain_id: 1,
     rpc_url: "https://rpc.builder0x69.io",
     block_chain: "eth",
   },
-  {
+  137: {
     name: "Polygon",
     chain_id: 137,
     rpc_url: "https://polygon-bor.publicnode.com",
     block_chain: "polygon",
   },
-  {
+  80001: {
     name: "Polygon Mumbai",
     chain_id: 80001,
     rpc_url: "https://rpc-mumbai.maticvigil.com",
     block_chain: "polygon_mumbai",
   },
-];
+};
 
 export default chains;
