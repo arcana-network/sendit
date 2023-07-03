3b57c3d2321105f9c9a74d6be38e12f1fe9cfad6
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-07-03 13:01:02 +0530

    align text to left

--- src/components/notifcations.vue
+++ src/components/notifcations.vue
@@ -9,7 +9,7 @@
       <button class="text-xs">Mark all as read</button>
     </div>
     <div class="w-full divide-y-[1px] divide-philippine-gray">
-      <div class="flex flex-col space-y-1 p-4">
+      <div class="flex flex-col text-left space-y-1 p-4">
         <span class="text-sm">Received 0.15 ETH</span>
         <span class="text-xs">
           You have received 0.15 ETH from Karthik (YtjZy......CAzXr). Give
@@ -17,7 +17,7 @@
         </span>
         <span class="text-philippine-gray text-xs">09:15 PM</span>
       </div>
-      <div class="flex flex-col space-y-[1px] p-4">
+      <div class="flex flex-col text-left space-y-[1px] p-4">
         <span class="text-sm">Received 0.15 ETH</span>
         <span class="text-xs">
           You have received 0.15 ETH from Karthik (YtjZy......CAzXr). Give
@@ -25,7 +25,7 @@
         </span>
         <span class="text-philippine-gray text-xs">09:15 PM</span>
       </div>
-      <div class="flex flex-col space-y-[1px] p-4">
+      <div class="flex flex-col text-left space-y-[1px] p-4">
         <span class="text-sm">Received 0.15 ETH</span>
         <span class="text-xs">
           You have received 0.15 ETH from Karthik (YtjZy......CAzXr). Give
