67391f122630ef94d9353dc0fb82eb7d7b78c311
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-07-03 12:32:23 +0530

    create profile component

--- src/components/profile.vue
+++ src/components/profile.vue
@@ -1,24 +1,57 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { toRefs } from "vue";
+import useAuthStore from "@/stores/auth";
+import CopyIcon from "@/assets/images/icons/copy.svg";
+import { useToast } from "vue-toastification";
+
+const authStore = useAuthStore();
+const { userInfo } = toRefs(authStore);
+
+const toast = useToast();
+
+function splitWalletAddress(address: string) {
+  return `${address.slice(0, 6)}...${address.slice(-4)}`;
+}
+
+function copyToClipboard() {
+  const el = document.createElement("textarea");
+  el.value = userInfo.value.address;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
+  toast.success("Wallet address copied to clipboard");
+}
+</script>
 
 <template>
   <div
     class="form-card p-0 space-y-[1px] w-80 divide-y-[1px] divide-philippine-gray"
   >
-    <div class="w-full p-4">
-      <span class="text-base uppercase font-bold">Profile</span>
+    <div class="w-full p-4 flex justify-start">
+      <span class="text-base uppercase font-bold text-left">Profile</span>
     </div>
     <div class="space-y-4 w-full p-4">
       <div class="flex flex-col">
-        <span class="text-philippine-gray text-xs">Name</span>
-        <span class="text-sm">Karthik Nathan</span>
+        <span class="text-philippine-gray text-xs text-left">Name</span>
+        <span class="text-sm text-left">{{ userInfo.name }}</span>
       </div>
-      <div class="flex flex-col">
-        <span class="text-philippine-gray text-xs">Email ID</span>
-        <span class="text-sm">karthiknathan@gmail.com</span>
+      <div class="flex flex-col justify-start">
+        <span class="text-philippine-gray text-xs text-left">Email ID</span>
+        <span class="text-sm text-left">{{ userInfo.email }}</span>
       </div>
-      <div class="flex flex-col">
-        <span class="text-philippine-gray text-xs">Account Address</span>
-        <span class="text-sm">9i57i160.... 3738266</span>
+      <div class="flex flex-col justify-start">
+        <span class="text-philippine-gray text-xs text-left"
+          >Account Address</span
+        >
+        <div class="flex space-x-1">
+          <span class="text-sm text-left overflow-hidden">{{
+            splitWalletAddress(userInfo.address)
+          }}</span>
+          <button @click.prevent="copyToClipboard">
+            <img :src="CopyIcon" alt="copy" />
+          </button>
+        </div>
       </div>
       <div class="w-full">
         <button class="btn btn-submit-secondary text-sm w-full">Logout</button>
