49e5ba1ef32f55a7ab2835a8d7015305ba732eb1
Karthik Durai <iamkarthiksiddarth@gmail.com>
2023-07-06 12:46:43 +0530

    notifications (#10)
    * move copy to clipboard to utils dir
    
    * use copy to clipboard from util
    
    * move code and remove duplicates
    
    * implement logic to fetch notifications
    
    * implement notification
    
    * implement mark all read, add no notification state
    
    * implement shoutout
    
    * remove github import

---
+++ src/constants/notificationsContent.ts
@@ -0,0 +1,28 @@
+const notificationsContent = {
+  0: ({ points }) => ({
+    title: `You have earned ${points} points`,
+    body: "",
+    path: "Leaderboard",
+    shoutout: false,
+  }),
+  256: ({ from, value }) => ({
+    title: `Received ${value} from ${from}`,
+    body: `You have received ${value} from ${from}. Give a shoutout on Twitter to earn XP.`,
+    path: "History",
+    shoutout: true,
+  }),
+  2: {
+    title: "Invite Claimed",
+    body: "You friend has accepted your invite to join SendIt. You have earned 40 XP!",
+    path: "",
+    shoutout: false,
+  },
+  3: {
+    title: "Invite Claimed",
+    body: "Welcome to SendIt. You have earned 40 XP!",
+    path: "",
+    shoutout: false,
+  },
+};
+
+export default notificationsContent;
