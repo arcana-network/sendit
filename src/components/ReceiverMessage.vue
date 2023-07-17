2554cf609688a5666e86024b0593f3c6d5fb65c8
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-07-17 13:11:20 +0530

    update receiver flow text and cta

--- src/components/ReceiverMessage.vue
+++ src/components/ReceiverMessage.vue
@@ -1,23 +1,50 @@
 <script setup lang="ts">
 import Overlay from "@/components/overlay.vue";
 import useArcanaAuth from "@/use/arcanaAuth";
+import { addUserToWaitlist } from "@/services/waitlist.service";
+import { computed, onMounted, ref } from "vue";
+import useLoaderStore from "@/stores/loader";
+import { normaliseEmail } from "@/utils/normalise";
+import { useToast } from "vue-toastification";
 
-const emit = defineEmits(["dismiss"]);
+const loaderStore = useLoaderStore();
+const email = ref("");
+const toast = useToast();
 
-function handleShowWallet() {
+function showWallet() {
   const arcanaAuth = useArcanaAuth();
   arcanaAuth.getAuthInstance().showWallet();
+}
+
+onMounted(() => {
+  setTimeout(showWallet, 1000);
+});
+
+const disableSubmit = computed(() => {
+  return !email.value.trim();
+});
+
+async function handleUserSubmission() {
+  try {
+    loaderStore.showLoader("Adding to waitlist...");
+    const isSubmitted = await addUserToWaitlist(
+      normaliseEmail(email.value),
+      ""
+    );
+    if (!isSubmitted) toast.error("Cannot add to waitlist. Please try again.");
+  } catch (e) {
+    toast.error("Email already added in waitlist");
+  } finally {
+    loaderStore.hideLoader();
+  }
 }
 </script>
 
 <template>
   <Overlay>
     <div
-      class="max-w-[360px] w-screen bg-eerie-black rounded-[10px] border-1 border-jet flex flex-col relative p-4 gap-5"
+      class="max-w-[500px] w-screen bg-eerie-black rounded-[10px] border-1 border-jet flex flex-col relative p-4 gap-5"
     >
-      <button class="absolute right-4 top-4" @click.stop="emit('dismiss')">
-        <img src="@/assets/images/icons/close.svg" alt="close" />
-      </button>
       <div class="flex flex-col gap-5">
         <div class="flex flex-col justify-center items-center gap-4">
           <img
@@ -25,22 +52,41 @@ function handleShowWallet() {
             alt="success"
             class="w-[50px] aspect-square"
           />
-          <span class="font-[500] text-[20px] uppercase font-bold"
-            >Received Crypto</span
-          >
-          <span class="text-xs text-philippine-gray max-w-[320px] text-center">
-            You have received some tokens from a SendIt user. Please check out
-            the wallet in the bottom right corner.
+          <span class="font-[500] text-4xl">Cha-ching!</span>
+          <span class="text-2xl">You’ve got crypto!</span>
+          <span class="text-xl text-philippine-gray text-center">
+            The balance in your wallet has been updated. Loved how simple it was
+            to receive crypto? Join the waitlist to access SendIt below.
           </span>
         </div>
-        <div class="flex justify-end">
-          <button
-            class="uppercase w-full border-white border-1 rounded-[5px] text-xs font-bold px-8 py-3"
-            @click.stop="handleShowWallet"
+        <form class="flex flex-col space-y-1.5 w-full rounded-md">
+          <label for="email-waitlist" class="text-xs"
+            >Email ID for Waitlist</label
           >
-            Show Wallet
-          </button>
-        </div>
+          <div class="w-full rounded-md flex bg-dark-charcoal px-2">
+            <input
+              type="email"
+              class="flex-1 bg-transparent input text-lg text-white"
+              placeholder="you@gmail.com"
+              v-model.trim="email"
+            />
+            <button
+              class="flex items-center justify-center"
+              :disabled="disableSubmit"
+              :class="{
+                'cursor-not-allowed': disableSubmit,
+                'cursor-pointer': !disableSubmit,
+                'opacity-50': disableSubmit,
+              }"
+              @click.prevent="handleUserSubmission"
+            >
+              <img
+                src="@/assets/images/icons/arrow-right.svg"
+                alt="email login"
+              />
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   </Overlay>
