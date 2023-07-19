a95d9dbb70dba8f65709ef2d2866be79609a9bbf
karthikdurai <iamkarthiksiddarth@gmail.com>
2023-07-19 15:43:10 +0530

    add currency to chain list

--- src/constants/chainList.ts
+++ src/constants/chainList.ts
@@ -4,18 +4,21 @@ const chains = {
     chain_id: 1,
     rpc_url: "https://rpc.builder0x69.io",
     blockchain: "eth",
+    currency: "ETH",
   },
   137: {
     name: "Polygon",
     chain_id: 137,
     rpc_url: "https://polygon-bor.publicnode.com",
     blockchain: "polygon",
+    currency: "MATIC",
   },
   80001: {
     name: "Polygon Mumbai",
     chain_id: 80001,
     rpc_url: "https://rpc-mumbai.maticvigil.com",
     blockchain: "polygon_mumbai",
+    currency: "MATIC",
   },
 };
 
