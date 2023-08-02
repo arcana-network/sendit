17c651ec2ec8d132a03d50839f92a1000a7154be
mmjee <root@git.maharshi.ninja>
2023-08-02 19:03:48 +0530

    Reformat

--- src/stores/connection.ts
+++ src/stores/connection.ts
@@ -20,10 +20,10 @@ type Account = {
 };
 
 export class SocketError extends Error {
-    code: number
+    code: number;
     constructor(message: string, code: number) {
         super(message);
-        this.code = code
+        this.code = code;
     }
 }
 
