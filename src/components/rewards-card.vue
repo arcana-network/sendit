45ec9d1e1a9819d8383078f0eebd041cf39190f8
shrinathprabhu <shrinathprabhu.123@gmail.com>
2023-08-23 21:22:07 +0530

    Update cards in send tx screen

--- src/components/rewards-card.vue
+++ src/components/rewards-card.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import ArrowRightIcon from "@/assets/images/icons/arrow-right.svg";
-import ArrowStayUpdated from "@/assets/images/arrow-stay-updated.svg";
-import TextStayUpdated from "@/assets/images/text-stay-updated.svg";
+import { useRouter } from "vue-router";
 
 defineProps({
   reward: {
@@ -9,6 +8,8 @@ defineProps({
     required: true,
   },
 });
+
+const router = useRouter();
 
 const emits = defineEmits(["invite", "verify-follow"]);
 
@@ -18,39 +19,52 @@ function onClick(reward) {
   } else if (reward.task === "Follow") {
     window.open(reward.url, "_blank");
     emits("verify-follow");
+  } else if (reward.task === "Shoutout") {
+    router.push({ name: "History" });
   }
 }
 </script>
 
 <template>
-  <div>
-    <div v-if="reward.task === 'Follow'" class="relative | flex">
-      <img
-        :src="ArrowStayUpdated"
-        alt="stay updated"
-        class="absolute -top-16 right-24"
-      />
-      <img
-        :src="TextStayUpdated"
-        alt="stay updated"
-        class="absolute -top-16 -right-6"
-      />
+  <button
+    class="flex min-w-[360px] max-w-[360px] w-[360px] rounded-[10px] overflow-hidden border border-jet pr-5 bg-[#0e0e0e]"
+    :class="{ 'cursor-pointer': reward.task }"
+    @click.stop="onClick(reward)"
+  >
+    <div
+      class="flex h-full items-center justify-center p-6 bg-[#151515] border-r-jet border-r"
+    >
+      <img :src="reward.image" :alt="reward.name" class="w-8 h-8" />
+    </div>
+    <div class="flex justify-between gap-5 flex-grow w-full items-center">
+      <div class="flex flex-col text-start px-4 py-5">
+        <span class="text-base font-bold text-[#d8d8d8]">{{
+          reward.name
+        }}</span>
+        <span class="text-sm text-philippine-gray">{{
+          reward.description
+        }}</span>
+        <span v-if="reward.dailyLimit" class="text-sm text-philippine-gray"
+          ><strong>Daily Limit:</strong> {{ reward.dailyLimit }}XP</span
+        >
+        <span v-if="reward.bonus" class="text-sm text-philippine-gray mt-4"
+          ><strong>Bonus:</strong> {{ reward.bonus }}</span
+        >
+      </div>
     </div>
     <div
-      class="flex w-80 h-28 border-1 border-jet bg-[#0e0e0e] rounded-md overflow-hidden"
-      :class="{ 'cursor-pointer': reward.task }"
-      @click.stop="onClick(reward)"
+      class="flex items-center h-full ml-2"
+      v-if="reward.task"
+      @click="emits(reward.task)"
     >
-      <div class="bg-eerie-black p-4 flex items-center">
-        <img :src="reward.image" :alt="reward.name" class="w-8 h-8" />
-      </div>
-      <div class="space-y-1 flex-1 p-4">
-        <p>{{ reward.name }}</p>
-        <p class="text-xs text-philippine-gray">{{ reward.description }}</p>
-      </div>
-      <button class="p-4" v-if="reward.task" @click="emits(reward.task)">
-        <img :src="ArrowRightIcon" alt="" />
-      </button>
+      <img
+        :src="ArrowRightIcon"
+        class="w-5 h-5"
+        :class="{
+          'opacity-0': reward.task === 'Transact',
+        }"
+        alt=""
+      />
     </div>
-  </div>
+  </button>
 </template>
