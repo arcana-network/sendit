bafa90ae8201d6ce09e3b20c8c68cf471fb898f5
shrinathprabhu <shrinathprabhu.123@gmail.com>
2023-08-25 11:53:18 +0530

    Make earn rewards as first section

--- src/pages/private/rewards/index.vue
+++ src/pages/private/rewards/index.vue
@@ -18,15 +18,15 @@ const route = useRoute();
     >
       <router-link
         class="px-2 py-1 cursor-pointer rounded-[5px]"
-        :class="{ 'bg-[#141414] text-white': route.name === 'My Rewards' }"
-        :to="{ name: 'My Rewards' }"
-        >My Rewards</router-link
-      >
-      <router-link
-        class="px-2 py-1 cursor-pointer rounded-[5px]"
         :class="{ 'bg-[#141414] text-white': route.name === 'Earn XP' }"
         :to="{ name: 'Earn XP' }"
         >Earn XP</router-link
+      >
+      <router-link
+        class="px-2 py-1 cursor-pointer rounded-[5px]"
+        :class="{ 'bg-[#141414] text-white': route.name === 'My Rewards' }"
+        :to="{ name: 'My Rewards' }"
+        >My Rewards</router-link
       >
     </div>
     <router-view class="m-8 mt-5"></router-view>
