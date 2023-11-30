58d797723458e0668e4ed6f4a544f5e6f56741da
shrinathprabhu <shrinathprabhu.123@gmail.com>
2023-11-30 16:30:16 +0530

    Add republic announcement in sendit

--- src/pages/landing.vue
+++ src/pages/landing.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { defineAsyncComponent } from "vue";
+import { defineAsyncComponent, ref } from "vue";
 import LandingHeader from "@/components/landing/LandingHeader.vue";
 import LandingHero from "@/components/landing/LandingHero.vue";
 import LandingNumbers from "@/components/landing/LandingNumbers.vue";
@@ -25,6 +25,8 @@ const LandingFAQ = defineAsyncComponent(
 const LandingFooter = defineAsyncComponent(
   () => import("@/components/landing/LandingFooter.vue")
 );
+
+const isBannerClosed = ref(false);
 </script>
 
 <template>
@@ -33,6 +35,31 @@ const LandingFooter = defineAsyncComponent(
     id="landing-page"
     style="overflow-x: hidden"
   >
+    <div
+      class="relative flex justify-center items-center p-2 bg-[#16AD65] text-[14px] font-[500] px-4 text-center"
+      v-if="!isBannerClosed"
+    >
+      <div class="items-baseline flex-wrap justify-center mr-3">
+        $XAR Public Sale is Live on Republic
+        <a
+          href="https://republic.com/arcana/"
+          target="_blank"
+          rel="noopener"
+          class="btn-submit py-1 px-2 rounded-md font-bold border-none"
+          >Visit&nbsp;Republic</a
+        >
+        &nbsp;&nbsp;<a
+          class="underline cursor-pointer underline-offset-[4px] font-[500]"
+          href="https://t.me/ArcanaNetwork"
+          target="blank"
+          rel="noopener"
+          >Need help?</a
+        >
+      </div>
+      <button class="absolute right-4" @click.stop="isBannerClosed = true">
+        <img src="@/assets/images/icons/close.svg" alt="close" />
+      </button>
+    </div>
     <LandingHeader />
     <LandingHero />
     <LandingNumbers />
