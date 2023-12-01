b83d3f4f3012d4e0bd6c0e2b279134fe0cc23a4d
shrinathprabhu <shrinathprabhu.123@gmail.com>
2023-12-01 10:43:32 +0530

    Remove session replay

--- src/main.ts
+++ src/main.ts
@@ -39,13 +39,9 @@ if (import.meta.env.PROD && import.meta.env.VITE_SENTRY_DSN) {
         tracePropagationTargets: ["https://sendit.arcana.network"],
         routingInstrumentation: Sentry.vueRouterInstrumentation(router),
       }),
-      new Sentry.Replay(),
     ],
     // Performance Monitoring
     tracesSampleRate: 1.0, // Capture 100% of the transactions
-    // Session Replay
-    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
   });
 }
 
