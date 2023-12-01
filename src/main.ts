7e4636a4d24e310dd1e6623a7354052b95f0d88c
shrinathprabhu <shrinathprabhu.123@gmail.com>
2023-12-01 10:25:29 +0530

    Add sentry tracing to sendit

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import store from "@/stores";
 import { Buffer } from "buffer";
 import Toast from "vue-toastification";
+import * as Sentry from "@sentry/vue";
 
 import "vue-toastification/dist/index.css";
 import "@fontsource/caveat/700.css";
@@ -27,6 +28,26 @@ const toastOptions = {
 };
 
 const app = createApp(App);
+
+if (import.meta.env.PROD && import.meta.env.VITE_SENTRY_DSN) {
+  Sentry.init({
+    app,
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [
+      new Sentry.BrowserTracing({
+        // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+        tracePropagationTargets: ["https://sendit.arcana.network"],
+        routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      }),
+      new Sentry.Replay(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, // Capture 100% of the transactions
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
+}
 
 app.use(store);
 app.use(router);
