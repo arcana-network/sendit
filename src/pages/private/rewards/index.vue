138f76c9241497012dbc21d15f2bc666e198b1b2
shrinathprabhu <shrinathprabhu.123@gmail.com>
2024-04-26 13:04:58 +0530

    Hide rewards and xp related stuff

--- src/pages/private/rewards/index.vue
+++ src/pages/private/rewards/index.vue
@@ -9,25 +9,24 @@ const route = useRoute();
     <div class="flex flex-col p-8">
       <span class="text-3.5xl">Rewards</span>
       <span class="text-sm text-philippine-gray max-w-[540px]"
-        >Find all your rewards below. Earn more XP to increase your leaderboard
-        rank. Testnet transactions are worth ⅕ XP of mainnet transactions.</span
+        >Find all your rewards below.</span
       >
-      <span class="text-sm text-philippine-gray max-w-[720px] mt-4"
+      <!-- <span class="text-sm text-philippine-gray max-w-[720px] mt-4"
         ><strong
           >* Note: Earn Send XP for up to 50 transactions daily; no limits on
           10% bonus XP.</strong
         ></span
-      >
+      > -->
     </div>
     <div
       class="flex mx-8 my-0 bg-eerie-black max-w-max flex-wrap py-1 px-2 border border-jet rounded-[10px] text-philippine-gray text-sm"
     >
-      <router-link
+      <!-- <router-link
         class="px-2 py-1 cursor-pointer rounded-[5px]"
         :class="{ 'bg-[#141414] text-white': route.name === 'Earn XP' }"
         :to="{ name: 'Earn XP' }"
         >Earn XP</router-link
-      >
+      > -->
       <router-link
         class="px-2 py-1 cursor-pointer rounded-[5px]"
         :class="{ 'bg-[#141414] text-white': route.name === 'My Rewards' }"
